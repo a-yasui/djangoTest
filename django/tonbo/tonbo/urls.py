66aaf1bd2b318c75baa81bc5bde4206616207190
Atsushi Yasui @ MBA <a.yasui@gmail.com>
2013-09-02 17:25:52 +0900

    もう面倒になった…

--- django/tonbo/tonbo/urls.py
+++ django/tonbo/tonbo/urls.py
@@ -19,5 +19,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
